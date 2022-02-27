df8b60a645592433a0b14fcf041f65f51e173a92
minhtuan16 <minhtuantran1621@gmail.com>
2022-02-27 23:57:33 +0700

    upload

--- Section_10/src/bai3/main/MainStudent2.java
+++ Section_10/src/bai3/main/MainStudent2.java
@@ -7,20 +7,34 @@ import bai3.service.StudentServiceImpl;
 
 public class MainStudent2 {
 	public static void main(String[] args) {
-		
+
 		System.out.println("Nhap n-stu: ");
 		int n = new Scanner(System.in).nextInt();
-		
+
 		Student[] students = new Student[n];
 		StudentServiceImpl studentImpl = new StudentServiceImpl();
-		
-		for(int i = 0; i < students.length; i++) {
+
+		for (int i = 0; i < students.length; i++) {
 			students[i] = new Student();
 			studentImpl.input(students[i]);
 		}
-		
-		for(Student student : students) {
+
+		for (Student student : students) {
 			studentImpl.info(student);
 		}
-	} 
+
+		int max = students[0].getAge_Stu();
+		for (int i = 0; i < students.length; i++) {
+			if (max < students[i].getAge_Stu()) {
+				max = students[i].getAge_Stu();
+			}
+		}
+
+		System.out.println("\nSinh vien co tuoi lon nhat: ");
+		for (int i = 0; i < students.length; i++) {
+			if (students[i].getAge_Stu() == max) {
+				studentImpl.info(students[i]);
+			}
+		}
+	}
 }
--- Section_10/src/bai3/service/StudentService.java
+++ Section_10/src/bai3/service/StudentService.java
@@ -4,6 +4,6 @@ import bai3.model.Student;
 
 public interface StudentService {
 	void input(Student stu); // tat ca cac ham trong interface deu la ham abstract
-	
+
 	void info(Student stu);
 }
